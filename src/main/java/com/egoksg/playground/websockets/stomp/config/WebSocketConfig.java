1ec453816084f22fba315c56c591f10b0668f7fa
SungGyunKim <egoksg@naver.com>
2022-12-18 04:22:24 +0900

    https://spring.io/guides/gs/messaging-stomp-websocket/를 따라한 소스

---
+++ src/main/java/com/egoksg/playground/websockets/stomp/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.egoksg.playground.websockets.stomp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic"); // 접두사가 붙은 대상의 클라이언트에게 인사말 메시지를 다시 전달하는 간단한 메모리 기반 메시지 브로커
+		config.setApplicationDestinationPrefixes("/app"); // @MessageMapping 주석이 달린 메서드에 바인딩된 메시지의 접두사
+	}
+	
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry
+			.addEndpoint("/gs-guide-websocket") // 엔드포인트를 등록합니다.
+				.withSockJS(); // WebSocket을 사용할 수 없는 경우 대체 전송을 사용할 수 있도록 SockJS 폴백 옵션을 활성화합니다.
+	}
+
+}
